92968262b39b49a6d00fe89df5a5dbacde6c9402
David Cloutier <david.cloutier@logmein.com>
2025-03-23 17:51:22 -0400

    fix: runnable test from cli

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig, devices } from '@playwright/test'
 import dotenv from 'dotenv'
-import path from 'path'
 
-dotenv.config({ path: path.resolve(__dirname, '.env') })
+dotenv.config({ path: '.env' })
 
 export default defineConfig({
   testDir: './tests',
@@ -13,7 +12,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.WORKERS,
+  workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
